8fa425a8d5b5f9e47195171b66bee45b10a166f3
Kotes <51745529+KotesUA@users.noreply.github.com>
2021-12-18 16:57:50 +0200

    Create index page

--- PasswordStorage/app.py
+++ PasswordStorage/app.py
@@ -1,11 +1,33 @@
-from flask import Flask
+import flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/', methods=["GET", "POST"])
+def index():  # put application's code here
+    return flask.render_template("index.html")
+
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    data = request.get_json()
+    email = data["email"]
+    password = data["password"]
+
+    # TODO: make database and hashing
+
+    return "Registration completed, welcome to PasswordStorage!"
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    data = request.get_json()
+    email = data["email"]
+    password = data["password"]
+
+    # TODO: make login via database
+
+    return "Logging in completed, welcome to PasswordStorage!"
 
 
 if __name__ == '__main__':

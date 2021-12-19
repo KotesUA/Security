5023521c64462e0181737c6b9d7ff01754be69b9
Kotes <51745529+KotesUA@users.noreply.github.com>
2021-12-20 00:58:48 +0200

    Added XOR

--- WeakSalt/main.py
+++ WeakSalt/main.py
@@ -1,7 +1,20 @@
-def decode():
-    print("I'm stupid")
+import random
 
+
+def readIn():
+    with open("encrypted.txt") as text:
+        result = text.readlines()
+    return result
+
+
+def saveDecoded():
+    print()
+    # do something
 
 
 if __name__ == '__main__':
-    decode()
+    inText = readIn()
+    for i in range(20):
+        print([chr(ord(a) ^ ord(b)) for a, b in zip(random.choice(inText), random.choice(inText))])
+        i += 1
+    # TODO save letters that are resolved and repeat

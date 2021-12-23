bbb6255222ff71108e6d25d16f359ee5cb300249
Kotes <51745529+KotesUA@users.noreply.github.com>
2021-12-23 03:12:11 +0200

    Refactor done

--- PasswordStorage/controller.py
+++ PasswordStorage/controller.py
@@ -1,29 +1,22 @@
-from encryption import to_bytes, from_bytes, gen_hash, gen_dek, encrypt
-import secrets
+from encryption import *
 import sqlite3
-from cryptography.hazmat.primitives.ciphers.aead import ChaCha20Poly1305
 
 
 KEK = b'\x8d\xcfR\xda\x83\xe2\xa06fp\xa2n\xadS\xdc\xd7\xb8\x02\xe7F\x08h\t\xe6\xd1\xda\x97i\xec4x\x97'
 
 
 def register(email, password, number):
-    create_users()
     connection = sqlite3.connect('database')
     cursor = connection.cursor()
 
-    dek_nonce, DEK = gen_dek()
-    encrypted_dek = ChaCha20Poly1305(KEK).encrypt(dek_nonce, DEK, None)
-    nonce = secrets.token_bytes(12)
+    dek = gen_dek()
+    dek_nonce, encrypted_dek = encrypt(KEK, dek)
 
-    encrypted_password = ChaCha20Poly1305(DEK).encrypt(nonce, gen_hash(password), None)
-    encrypted_number = ChaCha20Poly1305(DEK).encrypt(nonce, to_bytes(number), None)
-    cursor.execute("INSERT INTO users (email,password,number,dek,dek_nonce,nonce) VALUES (?,?,?,?,?,?)", (email,
-                                                                                      encrypted_password,
-                                                                                      encrypted_number,
-                                                                                      encrypted_dek,
-                                                                                      dek_nonce,
-                                                                                      nonce))
+    pass_nonce, encrypted_password = encrypt(dek, gen_hash(password))
+    num_nonce, encrypted_number = encrypt(dek, to_bytes(number))
+
+    cursor.execute("INSERT INTO users (email,password,number,dek,dek_nonce,pass_nonce,num_nonce) VALUES (?,?,?,?,?,?,?)",
+                   (email, encrypted_password, encrypted_number, encrypted_dek, dek_nonce, pass_nonce, num_nonce))
     connection.commit()
     connection.close()
 
@@ -31,44 +24,17 @@ def register(email, password, number):
 def login(email, password):
     connection = sqlite3.connect('database')
     cursor = connection.cursor()
-    try:
-        cursor.execute("SELECT * FROM users WHERE email = ?", (email,))
-        record = cursor.fetchone()
-        DEK = ChaCha20Poly1305(KEK).decrypt(record[5], record[4], None)
-        result = gen_hash(password) == ChaCha20Poly1305(DEK).decrypt(record[6], record[2], None)
-    except Exception:
-        print(Exception)
-        connection.commit()
-        connection.close()
-        return False
+
+    cursor.execute("SELECT * FROM users WHERE email = ?", (email,))
+    record = cursor.fetchone()
+    print(record[5])
+    print(record[4])
+    dek = decrypt(KEK, record[5], record[4])
+    result = gen_hash(password) == decrypt(dek, record[6], record[2])
+
     connection.commit()
     connection.close()
     if not result:
         return result
     else:
-        return ChaCha20Poly1305(DEK).decrypt(record[6], record[3], None)
-
-
-def create_users():
-    connection = sqlite3.connect('database')
-    cursor = connection.cursor()
-    cursor.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY,email TEXT NOT NULL,password TEXT NOT NULL)")
-    connection.commit()
-    connection.close()
-
-
-def show_users():
-    connection = sqlite3.connect('database')
-    cursor = connection.cursor()
-    cursor.execute("SELECT email, password FROM users")
-    users = cursor.fetchall()
-    connection.close()
-    return users
-
-
-def drop_users():
-    connection = sqlite3.connect('database')
-    cursor = connection.cursor()
-    cursor.execute("DROP TABLE IF EXISTS users")
-    connection.commit()
-    connection.close()
+        return decrypt(record[6], record[3], None)
--- PasswordStorage/encryption.py
+++ PasswordStorage/encryption.py
@@ -3,9 +3,6 @@ import secrets
 from cryptography.hazmat.primitives.ciphers.aead import ChaCha20Poly1305
 
 
-KEK = b'\x8d\xcfR\xda\x83\xe2\xa06fp\xa2n\xadS\xdc\xd7\xb8\x02\xe7F\x08h\t\xe6\xd1\xda\x97i\xec4x\x97'
-
-
 def to_bytes(text):
     return text.encode("utf-8")
 
@@ -19,16 +16,14 @@ def gen_hash(text):
 
 
 def gen_dek():
-    dek_nonce = secrets.token_bytes(12)
-    DEK = ChaCha20Poly1305.generate_key()
-    return dek_nonce, DEK
+    dek = ChaCha20Poly1305.generate_key()
+    return dek
 
 
-def encrypt(nonce, text):
-    # TODO
-    print()
+def encrypt(key, text):
+    nonce = secrets.token_bytes(12)
+    return nonce, ChaCha20Poly1305(key).encrypt(nonce, text, None)
 
 
-def decrypt(nonce, data):
-    # TODO
-    print()
+def decrypt(key, nonce, data):
+    return ChaCha20Poly1305(key).decrypt(nonce, data, None)

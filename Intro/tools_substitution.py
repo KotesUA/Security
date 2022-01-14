f32a89230367e062627d52f47ac38a80df6ce908
Kotes <97124614+budanov20@users.noreply.github.com>
2022-01-14 22:06:08 +0200

    Minor refactor

--- Intro/tools_substitution.py
+++ Intro/tools_substitution.py
@@ -30,15 +30,16 @@ def readd_ngr4():
     return ngr4
 
 
-def random_gene():
-    return Gene(''.join(choice(string.ascii_uppercase) for x in range(26)))
+def random_genes(number):
+    return [Gene(''.join(choice(string.ascii_uppercase) for x in range(26))) for _ in range(number)]
 
 
-# Class to representation our gen
+# class to represent a gene
 class Gene:
     def __init__(self, data):
-        # дата это строка которую нам удалось расшифровать
+        # data stores ascii string
         self.data = data
+        # score stores self data value
         self.score = 0
 
     # crossover two parents to create two children
@@ -50,38 +51,28 @@ class Gene:
         for own_data, partner_data in zip(self.data, partner.data):
             is_good1 = own_data in array
             is_good2 = partner_data in array
-            if is_good1 and is_good2:
-                new_char = choice((own_data, partner_data))
-            elif is_good1:
-                new_char = own_data
-            elif is_good2:
-                new_char = partner_data
-            else:
-                array.pop()
+
+            new_char = choice((own_data, partner_data)) if is_good1 and is_good2 \
+                else own_data if is_good1 \
+                else partner_data if is_good2 else array.pop()
+
             child.data.append(new_char)
             array.discard(new_char)
         return child
 
-    # меняем две буквы в строке местами
+    # change two letters visa-versa
     def mutation(self):
         a = randint(0, 25)
         b = randint(0, 25)
         self.data[a], self.data[b] = self.data[b], self.data[a]
-        # for i in range(len(bitstring)):
-        #     # check for a mutation
-        #     if rand() < r_mut:
-        #         # flip the bit
-        #         bitstring[i] = 1 - bitstring[i]
 
     def get_score(self):
         return 0
 
 
-# genetic algorithm
-def genetic_algorithm(ciphertext, dictionary, gen_number):
-    gens = []
-    for i in range(gen_number):
-        g = random_gene()
-        gens.append(g)
+def substitution_crack(text):
+    genes = random_genes(1000)
+    genes = sorted(genes, key=lambda gene: gene.score)[:500]
 
-    gens = sorted(gens, key=lambda g: g.score)[:gen_number / 2]
+    for g in genes:
+        g.get_score()

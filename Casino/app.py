5dd1953647ccae8fded0a606dc5ed976b7fd2d53
Kotes <97124614+budanov20@users.noreply.github.com>
2022-01-16 20:31:10 +0200

    Refactor LCG

--- Casino/app.py
+++ Casino/app.py
@@ -7,12 +7,23 @@ LAST_ID = 1197
 M = (2 ** 32) // 2
 
 
-def connect():
-    pass
-
-
 def lcg_crack():
-    pass
+    n1, n2, n3 = [player.play('Lcg', 1, 1)['realNumber'] for _ in range(3)]
+
+    # https://stackoverflow.com/questions/4798654/modular-multiplicative-inverse-function-in-python
+    # mod_inverse = pow(delta1, M - 2, M)
+
+    # https://www.kite.com/python/answers/how-to-calculate-modular-multiplicative-inverse-in-python
+    a = ((n3 - n2) * mod_inverse((n2 - n1), M)) % M
+    b = (n2 - n1 * a) % M
+    print(f'A={a}, B={b}')
+
+    val = player.play('Lcg', 1, 1)['realNumber']
+    num = (a * n3 + b) % M
+
+    while player.money <= 1000000:
+        print(player.play('Lcg', player.money, num))
+        num = (a * num + b) % M
 
 
 def mt_crack():
@@ -36,24 +47,6 @@ if __name__ == '__main__':
     #     if num == num_casino:
     #         print('Win')
 
-    n1, n2, n3 = [player.play('Lcg', 1, 1)['realNumber'] for _ in range(3)]
-    delta1 = n2 - n1
-    delta2 = n3 - n2
 
-    # https://stackoverflow.com/questions/4798654/modular-multiplicative-inverse-function-in-python
-    # mod_inverse = pow(delta1, M - 2, M)
-
-    # https://www.kite.com/python/answers/how-to-calculate-modular-multiplicative-inverse-in-python
-    a = (delta2 * mod_inverse(delta1, M)) % M
-    b = (n2 - n1 * a) % M
-    print(f'A={a}, B={b}')
-
-    val = player.play('Lcg', 1, 1)['realNumber']
-    num = (a * val + b) % M
-    print(f'Next num = {num}')
-    print(player.play('Lcg', 999, num))
-    if player.money < 1000000:
-        num = (a * val + b) % M
-        print(player.play('Lcg', player.money, num))
 
 
